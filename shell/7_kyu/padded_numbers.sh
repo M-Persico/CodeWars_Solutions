2fe6a420adbeca65835286ec07aa70c9f786028a
CommonLouis <michael.a.persico@gmail.com>
2021-10-11 00:01:42 -0400

    First Shell solutions (Oct.11)

---
+++ shell/7_kyu/padded_numbers.sh
@@ -0,0 +1,8 @@
+# Michael Persico
+# Oct.10, 2021
+# Substituting Variables Into Strings: Padded Numbers
+# https://www.codewars.com/kata/51c89385ee245d7ddf000001
+
+#!usr/bin/bash
+
+printf "Value is %05d" $1
---
+++ shell/8_kyu/hello_world.sh
@@ -0,0 +1,17 @@
+# Michael Persico
+# Oct.10, 2021
+# Classic Hello World
+# https://www.codewars.com/kata/57036f007fd72e3b77000023
+
+#!usr/bin/bash
+
+#!/bin/bash
+function Solution
+{
+    function main
+    {
+        echo "Hello World!"
+    }
+    main
+}
+Solution
